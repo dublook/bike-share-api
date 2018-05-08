736ef4051ea38b604130ae7553552470a6a506bd
Kota 'dublook' FUTAMI <kota.futami@gmail.com>
2018-05-09 02:57:09 +0900

    Add a function for bike list

--- handler.js
+++ handler.js
@@ -25,3 +25,15 @@ module.exports.ports = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.bikes = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      result: event
+    }),
+  };
+
+  callback(null, response);
+};
