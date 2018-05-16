93f26a36bf1e59dd32758ef073d621bc8b394f51
Kota FUTAMI <kota.futami@gmail.com>
2018-05-16 15:57:27 +0900

    Remove duplicated lines on handler.js

--- handler.js
+++ handler.js
@@ -1,85 +1,55 @@
 'use strict';
 
 module.exports.ports = (event, context, callback) => {
-  const param = JSON.parse(event.body)
-  provideBikeShareApi(param).listPorts(param.AreaId)
-    .then((ports) => {
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          result: ports
-        }),
-      };
-
-      callback(null, response);
-    })
-    .catch((error) => {
-      callback(error);
-    });
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  const param = JSON.parse(event.body);
+  provideBikeShareApi(param)
+    .listPorts(param.AreaId)
+    .then(responseSuccess(callback))
+    .catch(responseError);
 };
 
 module.exports.bikes = (event, context, callback) => {
-  const param = JSON.parse(event.body)
-  provideBikeShareApi(param).listBikes(event.pathParameters.ParkingID)
-    .then((bikes) => {
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          result: bikes
-        }),
-      };
-
-      callback(null, response);
-    })
-    .catch((error) => {
-      // TODO improve error handling
-      callback(error);
-    });
+  const param = JSON.parse(event.body);
+  provideBikeShareApi(param)
+    .listBikes(event.pathParameters.ParkingID)
+    .then(responseSuccess(callback))
+    .catch(responseError);
 };
 
 module.exports.makeReservation = (event, context, callback) => {
-  const param = JSON.parse(event.body)
-  provideBikeShareApi(param).makeReservation(event.pathParameters.ParkingID)
-    .then((result) => {
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          result: result
-        }),
-      };
-
-      callback(null, response);
-    })
-    .catch((error) => {
-      // TODO improve error handling
-      callback(error);
-    });
+  const param = JSON.parse(event.body);
+  provideBikeShareApi(param)
+    .makeReservation(event.pathParameters.ParkingID)
+    .then(responseSuccess(callback))
+    .catch(responseError);
 };
 
 module.exports.cancelReservation = (event, context, callback) => {
-  const param = JSON.parse(event.body)
+  const param = JSON.parse(event.body);
   provideBikeShareApi(param).cancelReservation()
-    .then((result) => {
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          result: result
-        }),
-      };
-
-      callback(null, response);
-    })
-    .catch((error) => {
-      // TODO improve error handling
-      callback(error);
-    });
+    .then(responseSuccess(callback))
+    .catch(responseError);
 };
 
 function provideBikeShareApi(param) {
   const BikeShareApi = require('./bike-share-api.js');
   return new BikeShareApi(param.MemberID, param.Password);
-};
+}
 
+function responseSuccess(callback) {
+  return result => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        result: result
+      })
+    };
+
+    callback(null, response);
+  }
+}
+
+function responseError(error) {
+  // TODO improve error handling
+  callback(error);
+}
