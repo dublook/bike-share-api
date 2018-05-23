c76857edf5b1537b7aaad059a58686927ee8c781
Kota FUTAMI <kota.futami@gmail.com>
2018-05-23 18:52:31 +0900

    Test slack reservation cencellation formatter

--- test/slack-notificaiton-test.js
+++ test/slack-notificaiton-test.js
@@ -9,6 +9,19 @@ test('Format simple text', async t => {
   t.plan(3);
   const payload = await SlackNotification.formatSimpleText({msg: 'Hello!'});
   t.is(payload.text, '{"msg":"Hello!"}');
+  assertPayloadBase(t, payload);
+});
+
+test('Format reservation cancellation success message', async t => {
+  t.plan(3);
+  const payload = await SlackNotification.formatCancelReservation({
+    Message: 'Cancellation success'
+  });
+  t.is(payload.text, 'Cancellation success');
+  assertPayloadBase(t, payload);
+});
+
+function assertPayloadBase(t, payload) {
   t.is(payload.username, 'Bike Share API');
   t.is(payload.icon_emoji, ':bike:');
-});
+}
