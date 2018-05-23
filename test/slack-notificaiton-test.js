e00dafb51a0601ac917b8b62cd2b85d639378110
Kota FUTAMI <kota.futami@gmail.com>
2018-05-23 19:25:14 +0900

    Test slack make reservation formatter

--- test/slack-notificaiton-test.js
+++ test/slack-notificaiton-test.js
@@ -21,6 +21,24 @@ test('Format reservation cancellation success message', async t => {
   assertPayloadBase(t, payload);
 });
 
+test('Format make reservation success message', async t => {
+  t.plan(7);
+  const payload = await SlackNotification.formatMakeReservation({
+    Title: 'Reservation success',
+    Message: 'Go to port within 20 minutes',
+    BikeNo: 'XXX1234',
+    Passcode: '0000'
+  });
+  t.is(payload.text, 'Reservation success');
+  const attachment = payload.attachments[0];
+  t.is(attachment.color, '#81C784');
+  const field = attachment.fields[0];
+  t.is(field.title, '自転車番号: XXX1234 パスコード: 0000');
+  t.is(field.value, 'Go to port within 20 minutes');
+  t.is(field.short, false);
+  assertPayloadBase(t, payload);
+});
+
 function assertPayloadBase(t, payload) {
   t.is(payload.username, 'Bike Share API');
   t.is(payload.icon_emoji, ':bike:');
